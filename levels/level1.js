0b95f671601ca3400593a605ec94647fd3f61152
a13xl <106622336+a13xl@users.noreply.github.com>
2023-03-20 20:28:53 +0100

    added small chicken

--- levels/level1.js
+++ levels/level1.js
@@ -1,10 +1,17 @@
 let level1 = new Level([
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-    new Endboss()
-],
-[
+    new Chicken(400),
+    new Chicken(700),
+    new Chicken(1000),
+    new Chicken(1300),
+    new Chicken(1600),
+    new Chicken(1900),
+    new SmallChicken(550),
+    new SmallChicken(850),
+    new SmallChicken(1150),
+    new SmallChicken(1450),
+    new SmallChicken(1750),
+    new SmallChicken(2150)
+],[
     new Cloud(),
 ],
 [
@@ -30,42 +37,18 @@ let level1 = new Level([
     new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
     new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
     new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3),
+    new BackgroundObject('img/5_background/layers/air.png', 719 * 4),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 4),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 4),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 4),
+    new BackgroundObject('img/5_background/layers/air.png', 719 * 5),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 5),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 5),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 5)
 ]);
 
 // NOT WORKING
 // Load images
 function initLvl() {
-    level1 = new Level([
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Endboss()
-    ],
-    [
-        new Cloud()
-    ],
-    [
-        new BackgroundObject('img/5_background/layers/air.png', -719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
-    
-        new BackgroundObject('img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/air.png', 719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
-    
-        new BackgroundObject('img/5_background/layers/air.png', 719 * 2),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 2),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 2),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 2),
-        new BackgroundObject('img/5_background/layers/air.png', 719 * 3),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3),
-    ]);
+    // fill array
 }
--- models/small-chicken.class.js
+++ models/small-chicken.class.js
@@ -1,20 +1,20 @@
-class Chicken extends MoveableObject{
-    y = 360;
-    height = 60;
-    width = 70;
+class SmallChicken extends MoveableObject{
+    y = 380;
+    height = 40;
+    width = 50;
     hp = 5;
     
     IMAGES_WALKING = [
-        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
-        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
-        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ];
 
-    IMAGES_DEAD = 'img/3_enemies_chicken/chicken_normal/2_dead/dead.png';
+    IMAGES_DEAD = 'img/3_enemies_chicken/chicken_small/2_dead/dead.png';
 
 
     constructor(x) {
-        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
 
         this.x = x + Math.random() * 200;
