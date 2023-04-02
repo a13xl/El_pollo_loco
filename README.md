9a7ae911a7e2a2e138cbd43f8a93ccedda45e170
a13xl <106622336+a13xl@users.noreply.github.com>
2023-04-02 11:45:29 +0200

    added endscreen

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # El_pollo_loco
 
 Aufgaben:
-02. Sounds (wie laufen)
-08. Game Over Screen (Verlieren/Gewinnen)
-09. OPTIONAL: Fullscreen (canvas.requestFullscreen())
+
+01. Game Over Screen (Verlieren/Gewinnen)
+02. Responsive machen
--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 let mute = false;
+let click_sound = new Audio('audio/button.mp3');
+let won = false;
 
 function pointOutBtn(contrast, opacity) {
-    document.getElementById('start-end_background').style.filter = `contrast(${contrast})`;
-    document.getElementById('introOutroBtn').style.filter = `opacity(${opacity})`;
+    document.getElementById('startBackground').style.filter = `contrast(${contrast})`;
+    document.getElementById('introBtn').style.filter = `opacity(${opacity})`;
 }
 
 function startGame() {
     document.getElementById('startScreen').classList.add('d-none');
     document.getElementById('canvas').classList.remove('d-none');
+
+    if(!mute) {
+        click_sound.play();
+    }
 
     init();
 }
@@ -27,5 +33,14 @@ function toggleAudio() {
     } else {
         img.src = 'img/icons/audio.png';
         mute = false;
+    }
+}
+
+function endScreeenWinLose() {
+    let img = document.getElementById('win-loseImg');
+    if(won) {
+        img.src = 'img/9_intro_outro_screens/game_over/game over!_01.png';
+    } else {
+        img.src = 'img/9_intro_outro_screens/game_over/you lost_01.png';
     }
 }
